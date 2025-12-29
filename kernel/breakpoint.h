b720469fb803fcf9584875bde49e3d9d02e230cc
Frogskok123 <39944364+Frogskok123@users.noreply.github.com>
2025-12-29 21:16:41 +0500

    Update breakpoint.h

--- kernel/breakpoint.h
+++ kernel/breakpoint.h
@@ -6,8 +6,8 @@
 #include <linux/ptrace.h>
 #include <linux/kthread.h>
 
-// Массив для хранения активных брейкпоинтов (по одному на ядро или процесс)
-// В реальном проекте лучше использовать список или хеш-мапу
+// Массив для хранения активных брейкпоинтов (заглушка)
+// В реальном проекте здесь должен быть список или map
 static struct perf_event * __percpu *sample_hbp;
 
 // Обработчик срабатывания брейкпоинта
@@ -15,16 +15,11 @@ static void sample_hbp_handler(struct perf_event *bp,
                                struct perf_sample_data *data,
                                struct pt_regs *regs)
 {
-    // ЗДЕСЬ ВАША ЛОГИКА
-    // regs->regs[0] ... regs->regs[30] - регистры общего назначения
-    // regs->pc - адрес инструкции
-    
+    // Логируем срабатывание. 
+    // В regs->pc адрес инструкции, в regs->regs[0..30] регистры.
+    // %llx выводит 64-битное число в hex
     printk(KERN_INFO "JiangNight: BP hit at %llx! X0: %llx X1: %llx", 
            regs->pc, regs->regs[0], regs->regs[1]);
-
-    // Если это Watchpoint (на данные), просто логируем.
-    // Если это Breakpoint (на код), нужно аккуратно обработать продолжение исполнения,
-    // иначе будет вечный цикл срабатывания. Обычно perf сам отключает BP на один шаг.
 }
 
 // Установка брейкпоинта
@@ -32,20 +27,23 @@ int install_breakpoint(pid_t pid, uintptr_t addr, int type)
 {
     struct perf_event_attr attr;
     struct task_struct *task;
+    struct perf_event *bp; // Объявление переменной В НАЧАЛЕ функции
     
     hw_breakpoint_init(&attr);
     
     attr.bp_addr = addr;
     attr.bp_len = HW_BREAKPOINT_LEN_4;
     
-    // Тип: HW_BREAKPOINT_X (Execute), HW_BREAKPOINT_W (Write), HW_BREAKPOINT_RW
-    if (type == 0) attr.bp_type = HW_BREAKPOINT_X;
-    else if (type == 1) attr.bp_type = HW_BREAKPOINT_W;
-    else attr.bp_type = HW_BREAKPOINT_RW | HW_BREAKPOINT_R;
+    // Тип: 0=Execute, 1=Write, 2=Read/Write
+    if (type == 0) 
+        attr.bp_type = HW_BREAKPOINT_X;
+    else if (type == 1) 
+        attr.bp_type = HW_BREAKPOINT_W;
+    else 
+        attr.bp_type = HW_BREAKPOINT_RW | HW_BREAKPOINT_R;
 
-    // Настраиваем callback
     attr.sample_period = 1;
-    attr.precise_ip = 1; // Точный IP
+    attr.precise_ip = 1;
     
     rcu_read_lock();
     task = pid_task(find_vpid(pid), PIDTYPE_PID);
@@ -55,9 +53,7 @@ int install_breakpoint(pid_t pid, uintptr_t addr, int type)
     if (!task) return -ESRCH;
 
     // Регистрация события
-    // perf_event_create_kernel_counter(attr, cpu, task, callback, context)
-    // cpu = -1 (любое ядро), task = целевой процесс
-    struct perf_event *bp = perf_event_create_kernel_counter(
+    bp = perf_event_create_kernel_counter(
         &attr, -1, task, sample_hbp_handler, NULL
     );
 
@@ -70,8 +66,10 @@ int install_breakpoint(pid_t pid, uintptr_t addr, int type)
 
     printk(KERN_INFO "JiangNight: BP installed at %lx for PID %d", addr, pid);
     
-    // Сохраняем указатель bp, чтобы потом удалить (нужна глобальная структура хранения)
-    // Для примера просто вернем успех, но в реальности нужно сохранить bp в список
+    // Внимание: здесь мы теряем указатель 'bp' (утечка ресурса), 
+    // так как не сохраняем его в глобальный список для последующего удаления.
+    // Для теста ок, для продакшена нужно добавить механизм сохранения.
+    
     return 0;
 }
 
